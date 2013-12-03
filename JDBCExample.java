f3ae6af59239a60db32c692991bb52d88d510a1d
philipmward <philipmward@hotmail.com>
2013-12-03 12:06:23 -0700

    Create JDBCExample.java

---
+++ JDBCExample.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class JDBCExample
+{
+
+public static void main(String[] args)
+{
+	JFrame frame = new LoginOracleASU();
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+}
+
+}
